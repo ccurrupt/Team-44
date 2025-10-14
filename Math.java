0b096722bfef2dbfb6caa3c6adcec6217f4d4afb
ccurrupt <xasusgames@gmail.com>
2025-10-14 16:28:05 +0100

    Math
    s

---
+++ Math.java
@@ -0,0 +1,8 @@
+/********************************
+Developer: T
+University ID: 240067205
+Function: This function takes two inputs as integers and returns the sum
+********************************/
+int add(int x, int y){
+    x+y;
+}
